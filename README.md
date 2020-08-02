e8c2a82ad9513d22d24b604e77e99e89813cf480
jhonssegura <jhonssegura@outlook.com>
2020-08-01 20:39:52 -0500

    Método GET y detalle en Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# python-apis-consumer
+# Python APIs Consumer
+
+# Requisitos
+
+* Requests
+
+# Instalación
+
+    $ virtualenv -p /usr/bin/python3 entorno-virtual
+    $ entorno-virtual/bin/activate
+    $ pip install requests
+    $ python main.py
+
--- main.py
+++ main.py
@@ -4,4 +4,9 @@ if __name__ == '__main__':
     url = 'https://www.google.com.mx/'
     response = requests.get(url)
 
-    print(response)
+    if response.status_code == 200:
+        content = response.content
+
+        file = open('google.html', 'wb')
+        file.write(content)
+        file.close()
